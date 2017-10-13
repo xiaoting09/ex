c63ddf94173ef85d6ab0213c49161daa5cba65ab
xiaoting09 <15623407705@163.com>
2017-10-13 22:39:29 +0800

    修改异步通知

--- ex-web/src/main/java/com/xiao/ex/ExWebApplication.java
+++ ex-web/src/main/java/com/xiao/ex/ExWebApplication.java
@@ -1,5 +1,6 @@
 package com.xiao.ex;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -11,6 +12,7 @@ public class ExWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExWebApplication.class, args);
+
     }
 
 }
--- ex-web/src/main/java/com/xiao/ex/common/thread/ServiceExThread.java
+++ ex-web/src/main/java/com/xiao/ex/common/thread/ServiceExThread.java
@@ -4,8 +4,10 @@ import com.xiao.ex.common.SpringContextUtil;
 import com.xiao.ex.core.ExceptionService;
 import com.xiao.ex.core.vo.req.ExceptionVo;
 import com.xiao.ex.service.ExClientListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.logging.Logger;
@@ -18,6 +20,14 @@ import java.util.logging.Logger;
 public class ServiceExThread implements Runnable {
     private static Queue<ExceptionVo> queue = new LinkedList<ExceptionVo>();
     public static Logger log = Logger.getLogger(ServiceExThread.class.toString());
+    @Autowired
+    private ExClientListService exClientListService;
+
+    @PostConstruct
+    public void init() {
+        ServiceExThread thread = new ServiceExThread();
+        new Thread(thread).start();
+    }
 
     public static void addExceptionVo(ExceptionVo vo) {
         if (vo != null) {
@@ -31,7 +41,7 @@ public class ServiceExThread implements Runnable {
             if (queue != null && queue.size() > 0) {
                 for (int i = 0; i < queue.size(); i++) {
                     ExceptionVo poll = queue.poll();
-                    SpringContextUtil.getBean(ExClientListService.class).addExClinet(poll);
+                    exClientListService.addExClinet(poll);
                 }
             }
             try {
@@ -41,7 +51,6 @@ public class ServiceExThread implements Runnable {
             }
         }
     }
-
 
 
 }
